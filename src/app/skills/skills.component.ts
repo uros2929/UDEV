6c5c28a4f42cf359ee953fe334836b2474c8d22d
Uros Marinkovic <38292385+uros2929@users.noreply.github.com>
2019-01-24 15:43:55 +0100

    update portfolio, add skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  widthHTML = 1;
+  widthJS=1;
+  widthAngular=1;
+  widthBootstrap=1;
+ 
   constructor() { }
 
   ngOnInit() {
+   this.moveBar('scalePerHTML',95,this.widthHTML);
+   this.moveBar('scalePerJS',90,this.widthJS);
+   this.moveBar('scalePerAngular',70,this.widthAngular);
+   this.moveBar('scalePerBootstrap',85,this.widthBootstrap);
+  }
+
+  moveBar(id,widthPer,widthLang){
+    let setINTH=setInterval(()=>{
+      widthLang++;
+      document.getElementById(id).style.width=widthLang+'%';
+      if (widthLang==widthPer) {
+        clearInterval(setINTH)
+      }
+    },8)
   }
 
 }
