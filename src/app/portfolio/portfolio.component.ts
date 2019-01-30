34d95ee644f1b1c44a9a5d98580d041de2e48e7a
Uros Marinkovic <38292385+uros2929@users.noreply.github.com>
2019-01-30 23:23:23 +0100

    add  scroll for img in portfolio 1

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
@@ -7,9 +9,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  allImgCards = document.getElementsByClassName('portfolio');
+  indexForObj = 0;
+  imgObject = {
+    ospAngular: ['OSPangular.png', 'OSPangular2.png'],
+    ospElectron: ['OSPelectron1.png'],
+    typingSpeed: ['typingSpeed.png'],
+    minesWeeper: ['mines.png'],
+    ticTacToe: ['XO.png'],
+    caffeApp: ['caffe.png'],
+    gitHubAcc: ['GITacc.png']
   }
 
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+
+  }
+
+  imgsModalShow(event) {
+    event.preventDefault();
+    document.getElementById('modalShowImg').style.display = 'block';
+    let findedImgCard = event.target.className;
+    if (findedImgCard == 'card-img-topOSPangular') {
+      this.chooseImg(this.imgObject.ospAngular);
+    } if (findedImgCard == 'card-img-topOSPelectron1') {
+      this.chooseImg(this.imgObject.ospElectron);
+    } else if (findedImgCard == 'card-img-topTypingSpeed') {
+      this.chooseImg(this.imgObject.typingSpeed);
+    } else if (findedImgCard == 'card-img-topMines') {
+      this.chooseImg(this.imgObject.minesWeeper);
+    } else if (findedImgCard == 'card-img-topXO') {
+      this.chooseImg(this.imgObject.ticTacToe)
+    } else if (findedImgCard == 'card-img-topCaffe') {
+      this.chooseImg(this.imgObject.caffeApp)
+    } else if (findedImgCard == 'card-img-topGitAcc') {
+      this.chooseImg(this.imgObject.gitHubAcc)
+    }
+  }
+
+  chooseImg(obj: any[]) {
+    document.getElementById('showImg').style.backgroundImage = "url('../../assets/images/" + obj[this.indexForObj] + "')";
+
+    document.getElementById('nextImg').addEventListener('click', () => {
+      this.indexForObj++;
+      document.getElementById('showImg').style.backgroundImage = "url('../../assets/images/" + obj[this.indexForObj] + "')";
+      console.log(this.indexForObj)
+
+    })
+    document.getElementById('prevImg').addEventListener('click', () => {
+      this.indexForObj--;
+      document.getElementById('showImg').style.backgroundImage = "url('../../assets/images/" + obj[this.indexForObj] + "')";
+      console.log(this.indexForObj)
+
+    })
+  }
+
+  closeModalShowImgs() {
+    document.getElementById('modalShowImg').style.display = 'none';
+    this.indexForObj = 0;
+    location.reload();
+  }
+
+  // imgMinus(obj: any) {
+  //   document.getElementById('prevImg').addEventListener('click', () => {
+  //     this.indexForObj--;
+  //     document.getElementById('showImg').style.backgroundImage = "url('../../assets/images/" + obj[this.indexForObj] + "')";
+  //     console.log(this.indexForObj)
+
+  //   })
+  // }
+  // imgPlus(obj: any) {
+  //   document.getElementById('nextImg').addEventListener('click', () => {
+  //     this.indexForObj++;
+  //     document.getElementById('showImg').style.backgroundImage = "url('../../assets/images/" + obj[this.indexForObj] + "')";
+  //     console.log(this.indexForObj)
+
+  //   })
+
+  // }
 }
